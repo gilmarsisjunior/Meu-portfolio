9a2f4183c4cb9ee1de797d9ce020f8cac377e436
Gilmar Silva Santos Junior <gilmar.sisjunior@gmail.com>
2021-04-21 02:39:19 -0300

    subindo para o git

---
+++ javascript/javascript.js
@@ -0,0 +1,48 @@
+function chamaModal(){
+    let modal = document.querySelector('.bgnegro')
+    modal.style.display = 'block'
+}
+function fechaModal(){
+    let modal = document.querySelector('.bgnegro')
+    modal.style.display = 'none'
+}
+
+
+//MODAL2
+
+function chamaModal1(){
+    let modal = document.querySelector('.bgnegro1')
+    modal.style.display = 'block'
+}
+function fechaModal1(){
+    let modal = document.querySelector('.bgnegro1')
+    modal.style.display = 'none'
+}
+
+//MODAL3
+
+
+function chamaModal2(){
+    let modal = document.querySelector('.bgnegro2')
+    modal.style.display = 'block'
+}
+function fechaModal2(){
+    let modal = document.querySelector('.bgnegro2')
+    modal.style.display = 'none'
+}
+
+
+
+//MODAL4       
+
+
+function chamaModal3(){
+    let modal = document.querySelector('.bgnegro3')
+    modal.style.display = 'block'
+}
+function fechaModal3(){
+    let modal = document.querySelector('.bgnegro3')
+    modal.style.display = 'none'
+}
+
+
